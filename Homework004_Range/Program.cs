82dd1fc6a96bf3662f05a9844c3ac2de29e2b93e
«Evgeniia» <girlbysinka@mail.ru>
2022-08-05 23:53:04 +0300

    Homework004_Range

---
+++ Homework004_Range/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N. 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4" 2 -> " -2, -1, 0, 1, 2"
+Console.Clear();
+
+int a = new Random().Next(1,10);
+Console.WriteLine(a);
+
+int count = -a;
+
+while (count <= a)
+{
+    Console.Write(" ");
+    Console.Write(count);
+    count++;
+}
