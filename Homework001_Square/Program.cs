8868b0302a7073b995f3b2c9d572f354e5314e5e
«Evgeniia» <girlbysinka@mail.ru>
2022-08-05 18:04:55 +0300

    add Homework001_Square

---
+++ Homework001_Square/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт его квадрат (число умноженное на само себя).
+int numberA = new Random().Next(1,10);
+
+int result = numberA * numberA;
+Console.Write("квадрат числа ");
+Console.Write(numberA);
+Console.Write(" = ");
+Console.WriteLine(result);
