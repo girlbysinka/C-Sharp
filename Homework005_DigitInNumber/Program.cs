1ec769b40a864e85c4eba7538f1659c149a44467
«Evgeniia» <girlbysinka@mail.ru>
2022-08-06 00:03:29 +0300

    Homework005_DigitInNumber

---
+++ Homework005_DigitInNumber/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа. 456 -> 6 782 -> 2 918 -> 8
+Console.Clear();
+
+int a = new Random().Next(1,10);
+Console.Write(a);
+
+int b = new Random().Next(1,10);
+Console.Write(b);
+
+int c = new Random().Next(1,10);
+Console.WriteLine(c);
+
+    Console.Write("Последняя цифра в числе = ");
+    Console.Write(c);
