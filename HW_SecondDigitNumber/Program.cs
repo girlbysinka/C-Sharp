24b02fd16e5e0a890a695077272d297089e91a93
«Evgeniia» <girlbysinka@mail.ru>
2022-08-11 14:08:12 +0300

    add HW_SecondDigitNumber

---
+++ HW_SecondDigitNumber/Program.cs
@@ -0,0 +1,27 @@
+﻿/* Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+Console.Clear();
+int userNumber = PrintUserNumber();
+
+if (userNumber > 100 & userNumber < 999)
+    {
+    int digit = (userNumber / 10) % 10;
+    Console.Write($"Вторая цифра в введенном числе = {digit}");
+    }
+
+else
+    {
+        Console.Write("Введенное число не является трёхзначным.");
+    }
+
+int PrintUserNumber()
+    {
+        int userNumber = new int();
+        Console.WriteLine("Введите трёхзначное число: ");
+        userNumber = Convert.ToInt32(Console.ReadLine());
+      
+    return userNumber;
+    }
